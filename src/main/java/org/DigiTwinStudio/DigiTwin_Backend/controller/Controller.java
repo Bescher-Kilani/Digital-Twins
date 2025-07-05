d7f26f70d1f27868d78480a310418dcd47300601
Naamjad <namjad@web.de>
2025-07-05 19:29:46 +0200

    domain package

--- src/main/java/org/DigiTwinStudio/DigiTwin_Backend/controller/Controller.java
+++ src/main/java/org/DigiTwinStudio/DigiTwin_Backend/controller/Controller.java
@@ -1,4 +1,4 @@
-package org.DigiTwinStudio.DigiTwin_Backend.Controller;
+package org.DigiTwinStudio.DigiTwin_Backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
