5587f6d7a637877a4acb61720e61986ffd2249b3
Naamjad <namjad@web.de>
2025-07-06 12:50:41 +0200

    changed publishMetadata from value to data annotation

--- src/main/java/org/DigiTwinStudio/DigiTwin_Backend/domain/PublishMetadata.java
+++ src/main/java/org/DigiTwinStudio/DigiTwin_Backend/domain/PublishMetadata.java
@@ -1,11 +1,15 @@
 package org.DigiTwinStudio.DigiTwin_Backend.domain;
 
-import lombok.Value;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Value
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PublishMetadata {
     String author; // visible author name can be a user or organization
     String shortDescription; // description visible in the Marketplace
