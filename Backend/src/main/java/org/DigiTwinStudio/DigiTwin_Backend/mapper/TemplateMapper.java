c474baaa380574477e6f9c629a7a067fd383c883
Naamjad <namjad@web.de>
2025-07-13 23:51:21 +0200

    dto external template, wird nicht gebraucht. daher gelöscht. templateMapper angepasst

--- Backend/src/main/java/org/DigiTwinStudio/DigiTwin_Backend/mapper/TemplateMapper.java
+++ Backend/src/main/java/org/DigiTwinStudio/DigiTwin_Backend/mapper/TemplateMapper.java
@@ -1,10 +1,8 @@
 package org.DigiTwinStudio.DigiTwin_Backend.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.DigiTwinStudio.DigiTwin_Backend.domain.Template;
 import org.DigiTwinStudio.DigiTwin_Backend.dtos.TemplateDto;
-import org.DigiTwinStudio.DigiTwin_Backend.dtos.ExternalTemplateDto;
 
 @Mapper(componentModel = "spring")
 public interface TemplateMapper {
@@ -12,9 +10,4 @@ public interface TemplateMapper {
     TemplateDto toDto(Template template);
 
     Template fromDto(TemplateDto dto);
-
-    @Mapping(target = "json", source = "rawJson")
-    @Mapping(target = "pulledAt", expression = "java(java.time.LocalDateTime.now())")
-    @Mapping(target = "active", constant = "true")
-    Template fromExternal(ExternalTemplateDto ext);
 }
