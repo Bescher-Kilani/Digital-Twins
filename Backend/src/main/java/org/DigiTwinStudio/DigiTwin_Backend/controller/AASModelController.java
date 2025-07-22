194b3ebb65bb256faa26a06162e5b30066edc618
Naamjad <namjad@web.de>
2025-07-23 00:20:20 +0200

    removed try catch from aasmodelcontroller

--- Backend/src/main/java/org/DigiTwinStudio/DigiTwin_Backend/controller/AASModelController.java
+++ Backend/src/main/java/org/DigiTwinStudio/DigiTwin_Backend/controller/AASModelController.java
@@ -1,17 +1,14 @@
 package org.DigiTwinStudio.DigiTwin_Backend.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.DigiTwinStudio.DigiTwin_Backend.domain.AASModel;
 import org.DigiTwinStudio.DigiTwin_Backend.dtos.AASModelDto;
 import org.DigiTwinStudio.DigiTwin_Backend.dtos.PublishRequestDto;
 import org.DigiTwinStudio.DigiTwin_Backend.services.AASModelService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/models")
@@ -37,15 +34,9 @@ public class AASModelController {
      * @return modelDTO or Response with error if access denied or model not found
      */
     @GetMapping("/{modelId}")
-    public ResponseEntity<?> getAASModel(@PathVariable String modelId, @RequestHeader("userId") String userId) {
-        try {
-            AASModelDto dto = this.aasModelService.getById(modelId, userId);
-            return ResponseEntity.ok(dto); // HTTP 200 OK mit DTO
-        } catch (ResponseStatusException e) {
-            return ResponseEntity
-                    .status(e.getStatusCode())
-                    .body(e.getReason()); // "Model not found" oder "Access denied"
-        }
+    public ResponseEntity<AASModelDto> getAASModel(@PathVariable String modelId, @RequestHeader("userId") String userId) {
+        AASModelDto dto = this.aasModelService.getById(modelId, userId);
+        return ResponseEntity.ok(dto);
     }
 
     /**
@@ -66,14 +57,8 @@ public class AASModelController {
      * @return ResponseEntity with the saved model, or an error if validation failed or model could not be found
      */
     @PutMapping("/{modelId}/save")
-    public ResponseEntity<?> saveAASModel(@PathVariable String modelId,@RequestHeader("userId") String userId, @RequestBody AASModelDto dto) {
-        try {
-            return ResponseEntity.ok(this.aasModelService.saveModel(modelId, userId, dto));
-        } catch (ResponseStatusException e) {
-            return ResponseEntity
-                    .status(e.getStatusCode())
-                    .body(e.getReason());
-        }
+    public ResponseEntity<AASModelDto> saveAASModel(@PathVariable String modelId,@RequestHeader("userId") String userId, @RequestBody AASModelDto dto) {
+        return ResponseEntity.ok(this.aasModelService.saveModel(modelId, userId, dto));
     }
 
     /**
@@ -84,28 +69,16 @@ public class AASModelController {
      */
     @DeleteMapping("/{modelId}/delete")
     public ResponseEntity<String> deleteAASModel(@PathVariable String modelId, @RequestHeader("userId") String userId) {
-        try {
-            this.aasModelService.deleteModel(modelId, userId);
-            return ResponseEntity
-                    .status(HttpStatus.NO_CONTENT)
-                    .body("Deleted " + modelId);
-        } catch (ResponseStatusException e) {
-            return ResponseEntity
-                    .status(e.getStatusCode())
-                    .body(e.getReason());
-        }
+        this.aasModelService.deleteModel(modelId, userId);
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .body("Deleted " + modelId);
     }
 
     @PostMapping("/{modelId}/publish")
     public ResponseEntity<String> publishAASModel(@PathVariable String modelId, @RequestHeader("userId") String userId, @RequestBody PublishRequestDto request) {
-        try {
-            this.aasModelService.publishModel(modelId, userId, request);
-            return ResponseEntity.ok("Published " + modelId);
-        } catch (ResponseStatusException e) {
-            return ResponseEntity
-                    .status(e.getStatusCode())
-                    .body(e.getReason());
-        }
+        this.aasModelService.publishModel(modelId, userId, request);
+        return ResponseEntity.ok("Published " + modelId);
     }
 
 }
