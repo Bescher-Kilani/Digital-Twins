4883cdaeaf9ea2d42cd61a8d3cbd79a2290865a8
bescherkilani@gmail.com <bescherkilani@gmail.com>
2025-07-22 18:32:44 +0200

    mark GlobalExceptionHandler with @RestControllerAdvice so we can handle the exceptions globally without try and catch in controllers

--- Backend/src/main/java/org/DigiTwinStudio/DigiTwin_Backend/exceptions/GlobalExceptionHandler.java
+++ Backend/src/main/java/org/DigiTwinStudio/DigiTwin_Backend/exceptions/GlobalExceptionHandler.java
@@ -5,12 +5,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 
 import java.time.Instant;
 import java.util.Map;
 
-public class GlobalExceptionHandler extends RuntimeException {
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
