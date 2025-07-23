01acb1ea6ceac1877c854d979cc1e8bfcbd3f0e7
Naamjad <namjad@web.de>
2025-07-24 01:59:49 +0200

    added exportStoredModel to Export Controller

--- Backend/src/main/java/org/DigiTwinStudio/DigiTwin_Backend/controller/ExportController.java
+++ Backend/src/main/java/org/DigiTwinStudio/DigiTwin_Backend/controller/ExportController.java
@@ -1,13 +1,25 @@
 package org.DigiTwinStudio.DigiTwin_Backend.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.DigiTwinStudio.DigiTwin_Backend.domain.ExportFormat;
 import org.DigiTwinStudio.DigiTwin_Backend.services.ExportService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
+
 @RestController
-@RequestMapping
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class ExportController {
 
     private final ExportService exportService;
+
+    @GetMapping("/models/{ModelId}/export/{format}")
+    public ResponseEntity<byte[]> exportStoredModel(@PathVariable String ModelId, @PathVariable String format, Principal principal) {
+        // ToDo: Add Guest-logic
+        String userId = principal.getName();
+        return ResponseEntity.ok(this.exportService.exportStoredModel(ModelId, ExportFormat.valueOf(format)));
+    }
+
 }
