a41d72bd4225f28a107b97d6dde20267a3a6ea46
ForceBlood <barkinkoral@hotmail.com>
2025-07-26 20:15:59 +0200

    enable CORS support in SecurityConfig

--- Backend/src/main/java/org/DigiTwinStudio/DigiTwin_Backend/config/SecurityConfig.java
+++ Backend/src/main/java/org/DigiTwinStudio/DigiTwin_Backend/config/SecurityConfig.java
@@ -13,6 +13,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+        .cors()
+        .and()
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/public/**").permitAll()
                 .anyRequest().authenticated()
